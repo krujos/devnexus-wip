c314d3557adf948bb10aa9805098199590c7c82e
krujos <jkruck@pivotal.io>
2016-02-12 22:38:20 -0600

    Add log level settings

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,15 +1,18 @@
 package com.example;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.apache.log4j.Logger.getLogger;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class HelloController {
-
+    private Logger log = getLogger(HelloController.class);
     @RequestMapping(value = "/", method = GET)
     public String hello() {
+        log.info("I will be greeting you soon");
         return "hello dev nexus!";
     }
 }
