eae5f168516633bd6222efb248e6360ea12f44ad
krujos <jkruck@pivotal.io>
2016-02-13 11:16:05 -0600

    Let editor format sql

--- src/main/resources/db/migration/V1__create.sql
+++ src/main/resources/db/migration/V1__create.sql
@@ -1,6 +1,9 @@
 CREATE TABLE `greeting` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `date` bigint(20) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `id`   BIGINT(20)   NOT NULL AUTO_INCREMENT,
+  `date` BIGINT(20)   NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 83
+  DEFAULT CHARSET = utf8;
